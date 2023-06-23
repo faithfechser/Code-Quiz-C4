8265243fd9050c57628630fad3cb82b073d9658a
Faith Fechser <faithfechser@gmail.com>
2023-06-22 19:57:20 -0600

    create questions for quiz

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+// Quiz Questions
+const questions = [
+{
+    question: "Which of the following is a proper example of a data type?",
+    options: [
+        "Boolean",
+        "String",
+        "Integer",
+        "All of the above"
+    ],
+    answer: "All of the above"
+},
+{
+    question: "What is a collection of data elements called?",
+    options: [
+        "A Collelement",
+        "An Array",
+        "A Variable",
+        "A For-Loop"
+    ],
+    answer: ""
+},
+{
+    question: "A For-Loop is used to run a piece of code more than once.",
+    options: [
+        "True",
+        "False"
+    ],
+    answer: "True"
+},
+{
+    question: "What does API stand for?",
+    options: [
+        "Artificial Programming Intelligence",
+        "Apples Pineapples & Insects",
+        "Application Programming Interface",
+        "Application Pogging Interweb"
+    ],
+    answer: "Application Programming Interface"
+},
+{
+    question: "Select the correct git command to create a new empty file.",
+    options: [
+        "cd",
+        "mkdir",
+        "touch",
+        "ls"
+    ],
+    answer: "touch"
+}
+]
